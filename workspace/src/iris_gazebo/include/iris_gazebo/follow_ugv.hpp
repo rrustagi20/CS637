a27e210bbccc9113bc4c56ad0d0c04ccdbdabd71
ba-13 <b.a.13.official@gmail.com>
2022-11-15 00:17:21 +0530

    Update landing

--- workspace/src/iris_gazebo/include/iris_gazebo/follow_ugv.hpp
+++ workspace/src/iris_gazebo/include/iris_gazebo/follow_ugv.hpp
@@ -5,28 +5,34 @@
 #include <trajectory_msgs/MultiDOFJointTrajectoryPoint.h>
 #include <geometry_msgs/Pose.h>
 #include <queue>
+#include <string>
 #include <geometry_msgs/Transform.h>
 
-int buffer_size_queue = 3;
-bool flag = false;
-float gain_factor_for_push = 200.0;
+struct coord
+{
+    float x, y, z;
+};
+
 const int husky_index = 2;
+int buffer_size_queue = 3;
+float gain_factor_for_push = 200.0;
 std::queue<float> history_xy;
 float sum_xy = 0;
 
-float x_ugv2, y_ugv2;
-float x_ugv = 0, y_ugv = 0, z_ugv = 0.245 * 2, x_uav, y_uav, z_uav;
+coord ugv, ugv2, uav;
+
 void get_pose_ugv(const gazebo_msgs::ModelStatesConstPtr &msg)
 {
-    x_ugv2 = x_ugv, y_ugv2 = y_ugv;
-    x_ugv = msg->pose[husky_index].position.x;
-    y_ugv = msg->pose[husky_index].position.y;
-    // z_ugv = msg->pose[husky_index].position.z;
+    ugv2.x = ugv.x;
+    ugv2.y = ugv.y;
+    ugv.x = msg->pose[husky_index].position.x;
+    ugv.y = msg->pose[husky_index].position.y;
+    ugv.z = msg->pose[husky_index].position.z + 0.324; // value according to height of ugv
 }
 
 void get_pose_uav(const geometry_msgs::Pose::ConstPtr &msg)
 {
-    x_uav = msg->position.x;
-    y_uav = msg->position.y;
-    z_uav = msg->position.z;
+    uav.x = msg->position.x;
+    uav.y = msg->position.y;
+    uav.z = msg->position.z;
 }
--- workspace/src/iris_gazebo/src/follow_ugv.cpp
+++ workspace/src/iris_gazebo/src/follow_ugv.cpp
@@ -4,18 +4,25 @@ int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "follow_ugv");
     ros::NodeHandle nh;
-    // Create a private node handle for accessing node parameters.
-    ros::NodeHandle nh_private("~");
-    ros::Subscriber ugv_pose_sub = nh.subscribe("gazebo/model_states", 10, &get_pose_ugv);
-    ros::Subscriber uav_pose_sub = nh.subscribe("/firefly/ground_truth/pose", 10, &get_pose_uav);
-    ros::Publisher trajectory_pub = nh.advertise<trajectory_msgs::MultiDOFJointTrajectory>("/firefly/command/trajectory", 10);
-    ROS_INFO("Started UAV trajectory.");
 
+    ros::Subscriber uav_pose_sub;
+    ros::Subscriber ugv_pose_sub = nh.subscribe("gazebo/model_states", 10, &get_pose_ugv);
+    ros::Publisher trajectory_pub;
+
+    std::string mavPoseTopic, mavCmdTrajectoryTopic;
+    if (nh.getParam("mav_pose", mavPoseTopic))
+        uav_pose_sub = nh.subscribe(mavPoseTopic, 10, &get_pose_uav);
+    else
+        uav_pose_sub = nh.subscribe("/firefly/ground_truth/pose", 10, &get_pose_uav);
+    if (nh.getParam("mav_cmd_trajectory", mavCmdTrajectoryTopic))
+        trajectory_pub = nh.advertise<trajectory_msgs::MultiDOFJointTrajectory>(mavCmdTrajectoryTopic, 10);
+    else
+        trajectory_pub = nh.advertise<trajectory_msgs::MultiDOFJointTrajectory>("/firefly/command/trajectory", 10);
+
+    ROS_INFO("Started UAV trajectory.");
     // Wait for 5 seconds to let the Gazebo GUI show up.
     ros::Duration(3.0).sleep();
     ROS_INFO("Started Publishing\n");
-
-    // ros::Duration(5.0).sleep();
 
     ros::Rate rate(20);
     while (ros::ok())
@@ -23,7 +30,10 @@ int main(int argc, char *argv[])
         trajectory_msgs::MultiDOFJointTrajectory trajectory_msg;
         trajectory_msgs::MultiDOFJointTrajectoryPoint pt;
         geometry_msgs::Transform tf;
-        float x = (x_ugv - x_uav) / 2 + x_ugv, y = (y_ugv - y_uav) / 2 + y_ugv, z = (z_ugv - z_uav) / 2 + z_ugv;
+
+        float x = (ugv.x - uav.x) / 2 + ugv.x;
+        float y = (ugv.y - uav.y) / 2 + ugv.y;
+        float z = (ugv.z - uav.z) / 2 + ugv.z;
         float xy_radius = x * x + y * y;
         history_xy.push(xy_radius);
         float popped = 0;
@@ -35,21 +45,24 @@ int main(int argc, char *argv[])
         sum_xy += xy_radius;
         sum_xy -= popped;
 
-        if (abs(sum_xy / buffer_size_queue - xy_radius) < 1e-1)
+        if (abs(sum_xy / buffer_size_queue - xy_radius) < 5e-2)
         {
             tf.translation.z = z;
-            x = x_ugv + gain_factor_for_push * (x_ugv - x_ugv2), y = y_ugv + gain_factor_for_push * (y_ugv - y_ugv2);
+            x = ugv.x + gain_factor_for_push * (ugv.x - ugv2.x), y = ugv.y + gain_factor_for_push * (ugv.y - ugv2.y);
         }
         else
-            tf.translation.z = z_uav;
+        {
+            tf.translation.z = ugv.z + 0.4; // for maintaining a fixed height until hovering just above the platform
+        }
 
         tf.translation.x = x;
         tf.translation.y = y;
-        ROS_INFO("UGV: %f %f %f", x_ugv, y_ugv, z_ugv);
-        ROS_INFO("UAV: %f %f %f", x_uav, y_uav, z_uav);
+        ROS_INFO("UGV: %f %f %f", ugv.x, ugv.y, ugv.z);
+        ROS_INFO("UAV: %f %f %f", uav.x, uav.y, uav.z);
         ROS_INFO("To go to: %f %f %f", x, y, z);
         if (tf.translation.z < 0.45)
-            ROS_DEBUG("Below threshold!");
+            ROS_DEBUG("Below threshold height!");
+
         pt.transforms.push_back(tf);
         trajectory_msg.points.push_back(pt);
         trajectory_pub.publish(trajectory_msg);
